9d059cc979e8914e3fe32ac52e461d8a1c2ce3cc
Hisanobu Araki <5218022+HisaAraki@users.noreply.github.com>
2026-04-17 04:04:34 +0900

    作業内容のメッセージ

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace AcrossReportDesigner
     internal class Program
     {
         private const string MutexName = "AcrossReportDesigner_SingleInstance";
+
         [STAThread]
         public static void Main(string[] args)
         {
@@ -20,22 +21,33 @@ namespace AcrossReportDesigner
             BuildAvaloniaApp()
                 .StartWithClassicDesktopLifetime(args);
         }
+
         private static void ShowMessage()
         {
-            // Windows専用
-            System.Runtime.InteropServices.Marshal
-                .ThrowExceptionForHR(
-                    MessageBox(IntPtr.Zero,
-                               "すでに起動しています。",
-                               "Across Report Designer",
-                               0));
+            if (OperatingSystem.IsWindows())
+            {
+                System.Runtime.InteropServices.Marshal
+                    .ThrowExceptionForHR(
+                        MessageBox(IntPtr.Zero,
+                                   "すでに起動しています。",
+                                   "Across Report Designer",
+                                   0));
+            }
+            else
+            {
+                Console.WriteLine("すでに起動しています。");
+            }
         }
+
+#pragma warning disable CA1416
         [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Unicode)]
         private static extern int MessageBox(
             IntPtr hWnd,
             string text,
             string caption,
             int type);
+#pragma warning restore CA1416
+
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
@@ -43,3 +55,4 @@ namespace AcrossReportDesigner
                 .LogToTrace();
     }
 }
+
